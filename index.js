7255596f65b7e2febe7b5bd907c38f96fefe04f5
MatheusEngSoft <matheus.engsof@gmail.com>
2024-02-23 13:48:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+function weaver() {
+  // Coletando os dados do input
+  let name = document.querySelector('#primeiroNome').value;
+  let email = document.querySelector('#email').value;
+  let cep = document.querySelector('#cep').value;
+  let latitude = document.querySelector('#latitude').value;
+  let longitude = document.querySelector('#longitude').value;
+
+  // consumo da api viacep
+  apiSearchCep();
+  apiSearchTemp();
+
+  function apiSearchCep() {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`http error: status ${response.status}`);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        alert(error.message);
+      })
+      .then(response => {
+        displayCepResult(response);
+        clearInputFields(); // Limpar os campos de input após a execução
+      });
+  }
+
+  function apiSearchTemp() {
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`http error: status ${response.status}`);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        alert(error.message);
+      })
+      .then(response => {
+        displayTempResult(response);
+      });
+  }
+
+  function displayCepResult(cepInfo) {
+    document.querySelector('#cepNum').textContent = `${cepInfo.cep || 'N/A'}`;
+    document.querySelector('#localidade').textContent = `${cepInfo.localidade || 'N/A'}`;
+    document.querySelector('#bairro').textContent = `${cepInfo.bairro === '' ? 'N/A' : cepInfo.bairro}`;
+    document.querySelector('#uf').textContent = `${cepInfo.uf || 'N/A'}`;
+    document.querySelector('#regiao').textContent = `${cepInfo.localidade || 'N/A'}`;
+  }
+
+  function displayTempResult(temperatureInfo) {
+    const currentTemperature = temperatureInfo.current_weather ? temperatureInfo.current_weather.temperature : 'N/A';
+    document.querySelector('#temperatura').textContent = `${currentTemperature} ºC`;
+  }
+
+  function clearInputFields() {
+    document.querySelector('#primeiroNome').value = '';
+    document.querySelector('#email').value = '';
+    document.querySelector('#cep').value = '';
+    document.querySelector('#latitude').value = '';
+    document.querySelector('#longitude').value = '';
+  }
+}
